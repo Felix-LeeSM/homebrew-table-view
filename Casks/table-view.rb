39d661a60a8603352390627c3d79e9afa29cd988
Felix Lee <haradwaith@naver.com>
2026-06-25 11:19:08 +0900

    chore(homebrew): update table-view cask v0.3.2 (#3)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/table-view.rb
+++ Casks/table-view.rb
@@ -1,11 +1,11 @@
 cask "table-view" do
-  version "0.3.1"
+  version "0.3.2"
 
   name "Table View"
   desc "Table View for MongoDB and SQL databases."
   homepage "https://github.com/Felix-LeeSM/table-view"
-  sha256 "be8a9411c44878f37570aea46216c8a2a6f8dbe36fc3d378b7c5eeb47b6efd3d"
-  url "https://github.com/Felix-LeeSM/table-view/releases/download/v0.3.1/Table.View_0.3.1_aarch64.dmg"
+  sha256 "301ebe1459e5875eb7050d8a8dfe6ffde7dc4731157063b471b0ef2c9496579d"
+  url "https://github.com/Felix-LeeSM/table-view/releases/download/v0.3.2/Table.View_0.3.2_aarch64.dmg"
   depends_on arch: :arm64
   app "Table View.app"
 end
